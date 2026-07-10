a23515f2068b7731a3d7ea8e993dd97a1633c26c
Lxxr <counull@outlook.com>
2026-07-10 21:24:41 +0800

    feat(project): integrate Luban data pipeline and restructure scripts

---
+++ DataTables/gen.bat
@@ -0,0 +1,14 @@
+set WORKSPACE=..
+set PROJECT_NAME=TinySpire
+set LUBAN_DLL=%WORKSPACE%\Tools\Luban\Luban.dll
+set CONF_ROOT=.
+
+dotnet %LUBAN_DLL% ^
+    -t all ^
+    -d json2 ^
+    -c cs-newtonsoft-json ^
+    --conf %CONF_ROOT%\luban.conf ^
+    -x outputCodeDir=%WORKSPACE%\%PROJECT_NAME%\Assets\Scripts\Runtime\Generated\Config ^
+    -x outputDataDir=%WORKSPACE%\%PROJECT_NAME%\Assets\TableDatas
+
+pause
